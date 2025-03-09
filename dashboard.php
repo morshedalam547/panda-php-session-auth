5bfc566c9f6726e40db7bb6527d1045c3e6959d5
M Abdus Shakur <mabdusshakur12@gmail.com>
2025-03-09 23:46:49 +0600

    Just some card showcase

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,91 @@ include 'includes/header.php';
 
 // Nav Bar 
 include 'includes/navbar.php';
+
+
+
+// all products
+
+$products = [
+    [
+        'title' => 'Panda T-Shirt',
+        'description' => 'This is dedicated to all panda fans. Get your panda t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Fox T-Shirt',
+        'description' => 'This is dedicated to all Fox fans. Get your Fox t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Cat T-Shirt',
+        'description' => 'This is dedicated to all Cat fans. Get your Cat t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Cat T-Shirt',
+        'description' => 'This is dedicated to all Cat fans. Get your Cat t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Cat T-Shirt',
+        'description' => 'This is dedicated to all Cat fans. Get your Cat t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Cat T-Shirt',
+        'description' => 'This is dedicated to all Cat fans. Get your Cat t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Cat T-Shirt',
+        'description' => 'This is dedicated to all Cat fans. Get your Cat t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+    [
+        'title' => 'Lion T-Shirt',
+        'description' => 'This is dedicated to all Lion fans. Get your Lion t-shirt now.',
+        'image' => 'https://placehold.co/400x400'
+    ],
+];
+
+
+
 ?>
 
 <!-- Dashboard Content -->
@@ -20,6 +105,58 @@ include 'includes/navbar.php';
                 You are not logged in.
             </h1>
         <?php endif; ?>
+    </div>
+</div>
+
+
+<div class="mt-5">
+    <div class="row g-5">
+
+        <?php
+        $counter = 0;
+        $colNum = 12;
+
+        foreach ($products as $product):
+
+            if ($counter == 1) {
+                $colNum = 6;
+            } elseif ($counter == 2) {
+                $colNum = 4;
+            } elseif ($counter > 3 && $counter < 5) {
+                $colNum = 3;
+            } elseif ($counter > 6 && $counter < 11) {
+                $colNum = 2;
+            }
+
+            ?>
+
+            <div class="col-md-<?php echo $colNum ?>">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php echo $product['image']; ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $product['title']; ?></h5>
+                        <p class="card-text">
+                            <?php echo $product['description']; ?>
+                        </p>
+                        <a href="#" class="btn btn-primary">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <?php
+            $counter++;
+        endforeach;
+        ?>
+
+
+        <p>
+            <?php
+            echo $counter;
+            ?>
+        </p>
+
+
     </div>
 </div>
 
